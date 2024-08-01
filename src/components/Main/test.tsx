59e198b08b0a955ecdaf4727eef0b5754101ea44
Wesley Anjos <wanjos.andrade@gmail.com>
2024-07-31 21:59:31 -0300

    chore: config snapshots com Jest e Styled Components

--- src/components/Main/test.tsx
+++ src/components/Main/test.tsx
@@ -4,11 +4,22 @@ import Main from '.'
 describe('<Main />', () => {
   it('should render the heading', () => {
     // renderizar o component
-    render(<Main />)
+    const { container } = render(<Main />)
 
     // busca o elemento e verificar a existencia
     expect(
       screen.getByRole('heading', { name: /react avancado/i })
     ).toBeInTheDocument()
+
+    // snapshot
+    expect(container.firstChild).toMatchSnapshot()
+  })
+
+  it('should render the colors correctly', () => {
+    // render component
+    const { container } = render(<Main />)
+
+    // check color
+    expect(container.firstChild).toHaveStyle({ 'background-color': '#06092b' })
   })
 })
