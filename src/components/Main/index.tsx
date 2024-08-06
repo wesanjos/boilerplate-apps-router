1f93823b87aae3b0be43331fa4aa94724f47ac9a
Wesley Anjos <wanjos.andrade@gmail.com>
2024-08-05 22:36:19 -0300

    chore: config storybook

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,8 +1,20 @@
 import * as S from './styles'
 
-const Main = () => (
+const Main = ({
+  title = 'React Avancado',
+  description = 'Typescript, ReactJS, NextJS e Styled Components'
+}) => (
   <S.Wrapper>
-    <h1>Curso React Avancado</h1>
+    <S.Logo
+      src="/img/logo.svg"
+      alt="Imagem de um atomo e React Avancado escrito ao lado."
+    />
+    <S.Title>{title}</S.Title>
+    <S.Description>{description}</S.Description>
+    <S.Illustration
+      src="/img/hero-illustration.svg"
+      alt="Um desenvolvedor de frente para uma tela de codigo."
+    />
   </S.Wrapper>
 )
 
